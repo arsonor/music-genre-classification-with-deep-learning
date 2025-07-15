a87302183a5c17ab2364b9d34b6769776a52372c
Martin Dornic <martin.dornic@gmail.com>
2025-07-15 14:31:58 +0200

    MLflow setup

---
+++ classifier/src/model.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+from tensorflow import keras
+
+def build_model(input_shape, l2_reg, learning_rate):
+    model = keras.Sequential([
+        keras.layers.Input(shape=input_shape),
+
+        keras.layers.Conv2D(32, (3,3), activation="relu",
+            kernel_regularizer=keras.regularizers.l2(l2_reg)),
+        keras.layers.MaxPool2D((3,3), strides=(2,2), padding="same"),
+        keras.layers.BatchNormalization(),
+
+        keras.layers.Conv2D(64, (3,3), activation="relu",
+            kernel_regularizer=keras.regularizers.l2(l2_reg)),
+        keras.layers.MaxPool2D((3,3), strides=(2,2), padding="same"),
+        keras.layers.BatchNormalization(),
+
+        keras.layers.Conv2D(128, (2,2), activation="relu",
+            kernel_regularizer=keras.regularizers.l2(l2_reg)),
+        keras.layers.MaxPool2D((2,2), strides=(2,2), padding="same"),
+        keras.layers.BatchNormalization(),
+
+        keras.layers.Flatten(),
+        keras.layers.Dense(128, activation="relu",
+            kernel_regularizer=keras.regularizers.l2(l2_reg)),
+        keras.layers.Dropout(0.4),
+
+        keras.layers.Dense(10, activation="softmax")
+    ])
+    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+    model.compile(
+        optimizer=optimizer,
+        loss="sparse_categorical_crossentropy",
+        metrics=["accuracy"],
+    )
+    return model
